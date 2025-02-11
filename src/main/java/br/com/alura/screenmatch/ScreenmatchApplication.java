e28e72fef2001b229f7c1a975be395fd563a19cb
Davi Macedo <134751279+DaviMacedo07@users.noreply.github.com>
2025-02-11 08:14:51 -0300

    Ajeitando o projeto para  a prod final

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,28 +1,12 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.principal.Principal;
-import br.com.alura.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-	private final SerieRepository serieRepository;
-	private final Principal principal;
+public class ScreenmatchApplication {
 
-	public ScreenmatchApplication(SerieRepository serieRepository, Principal principal) {
-		this.serieRepository = serieRepository;
-		this.principal = principal;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		principal.exibeMenu();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class,args);
+    }
 }
---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch;
+
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//	private final SerieRepository serieRepository;
+//	private final Principal principal;
+//
+//	public ScreenmatchApplication(SerieRepository serieRepository, Principal principal) {
+//		this.serieRepository = serieRepository;
+//		this.principal = principal;
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		principal.exibeMenu();
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplication.class, args);
+//	}
+//}
